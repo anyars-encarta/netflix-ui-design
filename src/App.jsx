6a2ab4ace19b4460312dd62672f6bf7adedb08b3
anyars-encarta <anyarsencarta@gmail.com>
2024-05-08 19:25:07 +0000

    Add register page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import './app.scss';
+import Register from './pages/register/Register';
 // import Home from './pages/home/Home';
 import Watch from './pages/watch/Watch';
 
 const App = () => {
   return (
     <div className="app">
-      <Watch />
+      <Register />
     </div>
   );
 }
