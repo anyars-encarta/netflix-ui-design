b3d33575906cade8fb6090cf56c0592028c806d2
anyars-encarta <anyarsencarta@gmail.com>
2024-05-08 18:06:19 +0000

    Add video preview

--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -1,18 +1,23 @@
 import { ArrowBackIosOutlined, ArrowForwardIosOutlined } from '@mui/icons-material'
 import './list.scss'
 import ListItem from '../listItem/ListItem'
-import { useRef } from 'react'
+import { useRef, useState } from 'react'
 
 const List = () => {
-const listRef = useRef();
+  const [moved, setMoved] = useState(false);
+  const [slideNumber, setSlideNumber] = useState(0);
+  const listRef = useRef();
 
   const handleClick = (direction) => {
+    setMoved(true)
     let distance = listRef.current.getBoundingClientRect().x - 50
-    if(direction === 'left') {
+    if (direction === 'left' && slideNumber > 0) {
+      setSlideNumber(slideNumber - 1);
       listRef.current.style.transform = `translateX(${230 + distance}px)`
     }
 
-    if(direction === 'right') {
+    if (direction === 'right' && slideNumber < 5) {
+      setSlideNumber(slideNumber + 1);
       listRef.current.style.transform = `translateX(${-230 + distance}px)`
     }
   }
@@ -21,20 +26,25 @@ const listRef = useRef();
     <div className='list'>
       <span className="listTitle">Continue to watch</span>
       <div className="wrapper">
-        <ArrowBackIosOutlined className='sliderArrow left' onClick={() => handleClick('left')} />
+        <ArrowBackIosOutlined
+          className='sliderArrow left'
+          onClick={() => handleClick('left')}
+          style={{ display: !moved && 'none' }}
+        />
 
-          <div className="container" ref={listRef}>
-            <ListItem />
-            <ListItem />
-            <ListItem />
-            <ListItem />
-            <ListItem />
-            <ListItem />
-            <ListItem />
-            <ListItem />
-            <ListItem />
-            <ListItem />
-          </div>
+        <div className="container" ref={listRef}>
+          <ListItem index={0} />
+          <ListItem index={1} />
+          <ListItem index={2} />
+          <ListItem index={3} />
+          <ListItem index={4} />
+          <ListItem index={5} />
+          <ListItem index={6} />
+          <ListItem index={7} />
+          <ListItem index={8} />
+          <ListItem index={9} />
+
+        </div>
 
         <ArrowForwardIosOutlined className='sliderArrow right' onClick={() => handleClick('right')} />
       </div>
--- src/components/listItem/ListItem.jsx
+++ src/components/listItem/ListItem.jsx
@@ -1,9 +1,61 @@
+import {
+  Add,
+  PlayArrow,
+  ThumbDownOutlined,
+  ThumbUpOffAltOutlined
+} from '@mui/icons-material';
 import './listItem.scss';
+import { useState } from 'react';
 
-const ListItem = () => {
+const ListItem = ({ index }) => {
+  const [hovered, setHovered] = useState(false);
+  const trailer = "https://vimeo.com/456293878";
+
   return (
-    <div className='listItem'>ListItem</div>
-  )
+    <div
+      className='listItem'
+      style={{ left: hovered && index * 225 - 50 + index * 2.5 }}
+      onMouseEnter={() => setHovered(true)}
+      onMouseLeave={() => setHovered(false)}
+    >
+      <img
+        src="https://occ-0-1723-92.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABU7D36jL6KiLG1xI8Xg_cZK-hYQj1L8yRxbQuB0rcLCnAk8AhEK5EM83QI71bRHUm0qOYxonD88gaThgDaPu7NuUfRg.jpg?r=4ee"
+        alt=""
+      />
+
+      {hovered && (
+        <>
+          {/* <video src={trailer} autoPlay={true} loop /> */}
+          <video controls autoPlay loop>
+            <source src={trailer} type="video/mp4" />
+          </video>
+
+          <div className="itemInfo">
+            <div className="icons">
+              <PlayArrow />
+              <Add />
+              <ThumbUpOffAltOutlined />
+              <ThumbDownOutlined />
+            </div>
+
+            <div className="itemInfoTop">
+              <span>1 hour 14 mins</span>
+              <span className='limit'>+16</span>
+              <span>1999</span>
+            </div>
+
+            <div className="desc">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+              Possimus, ullam ipsa natus excepturi aliquam molestiae dolor
+              quas numquam quasi
+            </div>
+
+            <div className="genre">Action</div>
+          </div>
+        </>
+      )}
+    </div>
+  );
 }
 
 export default ListItem
