2d08616820d7e971aeecb7b27202ff41bd4a056d
anyars-encarta <anyarsencarta@gmail.com>
2024-05-09 09:17:14 +0000

    Correct App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import './app.scss';
 import Login from './pages/login/Login';
 import Register from './pages/register/Register';
-// import Home from './pages/home/Home';
+import Home from './pages/home/Home';
 import Watch from './pages/watch/Watch';
 
 const App = () => {
   return (
     <div className="app">
-      <Login />
+      <Register />
     </div>
   );
 }
