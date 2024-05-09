16a76f063d15a5eb15d1d3ddcbed43a47ff28e3f
anyars-encarta <anyarsencarta@gmail.com>
2024-05-09 11:07:23 +0000

    Add watch page route

--- src/pages/watch/Watch.jsx
+++ src/pages/watch/Watch.jsx
@@ -1,18 +1,21 @@
 import { ArrowBackOutlined } from '@mui/icons-material';
+import { Link } from 'react-router-dom';
 import './watch.scss';
 
 const Watch = () => {
   return (
     <div className='watch'>
+      <Link to="/home" className='link'>
         <div className="back">
-            <ArrowBackOutlined />
-            Home
+          <ArrowBackOutlined />
+          Home
         </div>
+      </Link>
 
-        <video className="video" autoPlay progress controls>
-            <source src="https://vod-progressive.akamaized.net/exp=1624452918~acl=%2Fvimeo-prod-skyfire-std-us%2F01%2F2400%2F14%2F362003850%2F1486625955.mp4~hmac=d6f829e7bb83f1ee6a28047d00aa2c1083c8fe5036c8084a4adf1c3903085856/vimeo-prod-skyfire-std-us/01/2400/14/362003850/1486625955.mp4" type="video/mp4"/>
-            Your browser does not support the video tag.
-        </video>
+      <video className="video" autoPlay progress controls>
+        <source src="https://vod-progressive.akamaized.net/exp=1624452918~acl=%2Fvimeo-prod-skyfire-std-us%2F01%2F2400%2F14%2F362003850%2F1486625955.mp4~hmac=d6f829e7bb83f1ee6a28047d00aa2c1083c8fe5036c8084a4adf1c3903085856/vimeo-prod-skyfire-std-us/01/2400/14/362003850/1486625955.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
     </div>
   )
 }
